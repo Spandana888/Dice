d5f0d9be271a8cad130bad2d7774ca2cf383df7a
Spandana Chinnappa <56736951+Spandana888@users.noreply.github.com>
2022-11-02 20:40:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var randomNumber1 = Math.round((Math.random()*5)+1);
+var randomNumber2 = Math.round((Math.random()*5)+1);
+function play1(){
+    if(randomNumber1 === 1){
+        document.querySelectorAll("img")[0].setAttribute("src","./images/dice1.png");
+    }else if(randomNumber1 === 2){
+        document.querySelectorAll("img")[0].setAttribute("src","./images/dice2.png");
+    }else if(randomNumber1 === 3){
+        document.querySelectorAll("img")[0].setAttribute("src","./images/dice3.png");
+    }else if(randomNumber1 === 4){
+        document.querySelectorAll("img")[0].setAttribute("src","./images/dice4.png");
+    }else if(randomNumber1 === 5){
+        document.querySelectorAll("img")[0].setAttribute("src","./images/dice5.png");
+    }else{
+        document.querySelectorAll("img")[0].setAttribute("src","./images/dice6.png");
+    }
+}
+function play2(){
+    if(randomNumber2 === 1){
+        document.querySelectorAll("img")[1].setAttribute("src","./images/dice1.png");
+    }else if(randomNumber2 === 2){
+        document.querySelectorAll("img")[1].setAttribute("src","./images/dice2.png");
+    }else if(randomNumber2 === 3){
+        document.querySelectorAll("img")[1].setAttribute("src","./images/dice3.png");
+    }else if(randomNumber2 === 4){
+        document.querySelectorAll("img")[1].setAttribute("src","./images/dice4.png");
+    }else if(randomNumber2 === 5){
+        document.querySelectorAll("img")[1].setAttribute("src","./images/dice5.png");
+    }else{
+        document.querySelectorAll("img")[1].setAttribute("src","./images/dice6.png");
+    }
+}
+
+var player1 = play1();
+var player2 = play2()
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+}else{
+    document.querySelector("h1").innerHTML = "Draw!!";  
+}
